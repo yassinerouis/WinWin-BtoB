13dbf37d9df4dca6f67709402c64994004e0009f
yassinerouis <56519992+yassinerouis@users.noreply.github.com>
2019-12-16 22:01:31 +0100

    ajouter un utilisateur

---
+++ src/main/java/org/sid/entities/User.java
@@ -0,0 +1,84 @@
+package org.sid.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.Size;
+@Entity
+public class User implements Serializable {
+	@Id @GeneratedValue
+	Long id;
+	@Size(min = 3, max =30 )
+	String nom;
+	@Size(min = 3, max =30 )
+	String prenom;
+	@Column(length=12)
+	String telephone;
+	@Size(min = 10, max =30 )
+	String email;
+	@Size(min = 10, max =100 )
+	String adresse;
+	
+	String type;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public String getPrenom() {
+		return prenom;
+	}
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+	public String getTelephone() {
+		return telephone;
+	}
+	public void setTelephone(String telephone) {
+		this.telephone = telephone;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getAdresse() {
+		return adresse;
+	}
+	
+	public User() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public User(int id, String nom, String prenom, String telephone, String email, String adresse, String type) {
+		super();
+		this.id = id;
+		this.nom = nom;
+		this.prenom = prenom;
+		this.telephone = telephone;
+		this.email = email;
+		this.adresse = adresse;
+		this.type = type;
+	}
+	public void setAdresse(String adresse) {
+		this.adresse = adresse;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	
+}
---
+++ src/main/java/org/sid/web/UserController.java
@@ -0,0 +1,26 @@
+package org.sid.web;
+
+import org.sid.dao.UserRepository;
+import org.sid.entities.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class UserController {
+	@Autowired
+UserRepository userrepository;
+	@PostMapping("/saveuser")
+	public String saveUser(User user) {
+		userrepository.save(user);
+		return "addUser";
+	}
+	
+	@GetMapping("/")
+	public String formulaireAjout(Model model) {
+		model.addAttribute("user", new User());
+		return "addUser";
+	}
+}
